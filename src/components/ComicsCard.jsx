47767c9ada1871c81ccde76afc15e97bb74da21b
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-05-07 18:01:53 +0200

    [UPD] CArd CSS

--- src/components/ComicsCard.jsx
+++ src/components/ComicsCard.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 const ComicsCard = (props) => {
   return (
-   <div>
-       Singolo Fumetto
-      <h4>{props.comics.title}</h4>
-      <img src={props.comics.thumb} alt={"DC Comics"} />
-   </div>
+    <> 
+      <div className="comics-card"> 
+      <h5>{props.title}</h5>  
+        <img src={props.image} alt={"DC Comics"} />
+      </div>
+    </>
   );
 }
 
 export default ComicsCard;
 
+      /*chimata list card: 
+      <ComicsCard comics={comics[0]}/> */
+
+      // da riportar così
+      //  <h4>{props.comics.title}</h4>
+      // <img src={props.comics.image} alt={"DC Comics"} />
--- src/components/ListComics.jsx
+++ src/components/ListComics.jsx
@@ -160,6 +160,7 @@ const ListComics = () => {
     <div>
       <h1>List Comics</h1>
       <div className="comics-list">
+        {/* <ComicsCard comics={comics[0]}/> */}
         <ComicsCard title={comics[0].title} image={comics[0].thumb}/>
         <ComicsCard title={comics[1].title} image={comics[1].thumb}/>
         <ComicsCard title={comics[2].title} image={comics[2].thumb}/>
@@ -169,7 +170,9 @@ const ListComics = () => {
         <ComicsCard title={comics[6].title} image={comics[6].thumb}/>
         <ComicsCard title={comics[7].title} image={comics[7].thumb}/>
         <ComicsCard title={comics[8].title} image={comics[8].thumb}/>
-        <ComicsCard title={comics[9].title} image={comics[9].thumb}/>
+        <ComicsCard title={comics[9].title} image={comics[9].thumb}/> 
+        <ComicsCard title={comics[10].title} image={comics[10].thumb}/>
+        <ComicsCard title={comics[11].title} image={comics[11].thumb}/>
       </div>
     </div>   
   )
